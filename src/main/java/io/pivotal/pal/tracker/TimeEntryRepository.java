070cf01bd01ae1e0079383b530d1f7869475ecba
yeremia-d <yeremia.djaja@gmail.com>
2019-11-05 17:00:07 -0500

    Integrated JDBC

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -3,7 +3,7 @@ package io.pivotal.pal.tracker;
 import java.util.List;
 
 public interface TimeEntryRepository {
-     TimeEntry create(TimeEntry any);
+     TimeEntry create(TimeEntry timeEntry);
 
      TimeEntry find(Long timeEntryId);
 
